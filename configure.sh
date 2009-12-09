51f1fc03eed0061e9971758e5849756a42f6e29a
Jari Aalto <jari.aalto@cante.net>
2009-12-09 18:54:34 +0200

    import original  CYGWIN-PATCHES 2009-12-09

---
+++ configure.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# Copyright (C) YYYY Firstname Lastname; Licensed under GPL v2 or later
+#
+# configure.sh -- Custom configure script
+#
+#   Called as: CYGWIN-PATCHES/configure.sh <absolute-install-dir>
+
+PATH="/sbin:/usr/sbin/:/bin:/usr/bin"
+LC_ALL="C"
+
+Configure()
+{
+    instdir="$1"
+    instdir=${instdir%/}  # Remove trailing slash
+
+    if  [ -z  "$instdir" ] || [ ! -d $instdir ]; then
+        echo "$0: [ERROR] No such dir [$instdir]" >&2
+        exit 1
+    else
+        #   EXAMPLE: Any custom configuration options
+
+	cd gnu || return $?
+
+	if [ ! -f ./configure ]; then
+	    echo ">> Wait, generating ./configure with autoconf -f -i"
+	    autoconf -f -i
+	fi
+
+        ./configure                       \
+        --prefix=/usr                     \
+        --sysconfdir=/etc                 \
+        --libexecdir='${sbindir}'         \
+        --exec-prefix=/usr                \
+        --localstatedir=/var              \
+        --datadir='${prefix}/share'       \
+        --mandir='${prefix}/share/man'    \
+        --infodir='${prefix}/share/info'  \
+        --libdir='${prefix}/lib'          \
+        --includedir='${prefix}/include'
+
+    fi
+}
+
+Configure "$@"
+
+# End of file
